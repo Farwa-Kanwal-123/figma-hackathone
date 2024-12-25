a705cea706b5eefb830d045b7daed5a19f37f2b5
Farwa-Kanwal-123 <wahabzahid06@gmail.com>
2024-12-26 02:53:58 +0500

    updated

--- src/components/ourChefPage/Grid.tsx
+++ src/components/ourChefPage/Grid.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 
 const Grid = ({children}:{children:ReactNode}) => {
   return (
-    <div className='wrapper grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-[30px] space-y-1 my-[50px] md:my-[120px]'>
+    <div className='wrapper grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[30px] space-y-1 my-[50px] md:my-[120px]'>
       {children}
     </div>
   )
