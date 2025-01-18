df9cf77b1425350ccd1b10a543d7d9a74665ca85
Farwa-Kanwal-123 <wahabzahid06@gmail.com>
2025-01-18 21:02:16 +0500

    Updated folder name

--- src/app/(others)/our-chef/page.tsx
+++ src/app/(others)/our-chef/page.tsx
@@ -1,12 +1,71 @@
+// import React from 'react'
+// import Hero from "@/components/OthersHero";
+// import Chef from "@/components/ourChefPage/Chef";
+
+// const OurChef = () => {
+//   return (
+//     <div>
+//         <Hero heading="Our Chef"/>
+//         <Chef/>
+//     </div>
+//   )
+// }
+
+// export default OurChef;
+
+
+// fetch sanity data
 import React from 'react'
 import Hero from "@/components/OthersHero";
-import Chef from "@/components/ourChefPage/Chef";
+import {client} from '@/sanity/lib/client'
+import Image from 'next/image'
 
-const OurChef = () => {
+const getChef = async () =>{
+  const chefs = await client.fetch(
+    `
+    *[_type=="chef"]{
+    _id,
+    name,
+    position,
+    "image_url":image.asset->url
+}[]
+  `
+  )
+  return chefs;
+}
+
+const OurChef = async () => {
+  const chefs = await getChef();
+  // console.log(chefs)
+
   return (
     <div>
         <Hero heading="Our Chef"/>
-        <Chef/>
+        {/* <Grid/> */}
+         <div className='wrapper grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[30px] space-y-1 my-[50px] md:my-[120px]'>
+     {
+      chefs.map((chef)=>{
+        return(
+          <div className='w-full lg:w-[250px] h-[430px]' key={chef.id}>
+          <div className='w-full h-[365px] flex justify-center items-center'>
+          <Image 
+            src={chef.image_url} 
+            alt={chef.name} 
+            width={300}
+            height={300}
+  
+            className="w-full h-full object-cover" 
+          />
+          </div>
+          <div className='flex flex-col items-center w-full h-[65px]'>
+           <h2 className='font-[700] text-[20px]'>{chef.name}</h2>
+           <p>{chef.position}</p>
+          </div>
+      </div>
+       )
+      })
+     }
+   </div>
     </div>
   )
 }
